eeb45d290068fdae03ed878a4520b091dbfdeb04
Christopher Bimson <christopher.bimson>
2017-06-06 11:49:11 +0100

    Create an IOutputWriter factory and wire it up to the --outputFormat command line argument.

---
+++ Forecaster/Domain/OutputWriterFactory.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Forecaster.Domain
+{
+    static class OutputWriterFactory
+    {
+        private static readonly IDictionary<ForecastOutputFormat, IOutputWriter> OutputWriters
+            = new Dictionary<ForecastOutputFormat, IOutputWriter>
+            {
+                {ForecastOutputFormat.Pretty, new PrettyOutputWriter() },
+                {ForecastOutputFormat.Json, new JsonOutputWriter(Console.Out) },
+                {ForecastOutputFormat.Csv, new CsvOutputWriter(Console.Out) },
+            };
+
+        internal static IOutputWriter Create(ForecastOutputFormat format)
+        {
+            return OutputWriters[format];
+        }
+    }
+}
--- Forecaster/Program.cs
+++ Forecaster/Program.cs
@@ -18,7 +18,7 @@ namespace Forecaster
             var historicSamples = new HistoricSamples(new Random(), options.HistoricThroughput);
 
             var trials = Trials.Generate(historicSamples, options.PeriodsToForecast, options.NumberOfTrials);
-            trials.Summarize(new CsvOutputWriter(Console.Out));
+            trials.Summarize(OutputWriterFactory.Create(options.OutputFormat));
         }
     }
 }
