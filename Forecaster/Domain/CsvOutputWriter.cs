b8871f53733da1b838bf7c1f9f49e0a5e13fe61b
Christopher Bimson <christopher.bimson>
2017-06-06 10:01:41 +0100

    Implement a CSV Output Writer

---
+++ Forecaster/Domain/CsvOutputWriter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forecaster.Domain
+{
+    class CsvOutputWriter : IOutputWriter
+    {
+        private readonly TextWriter _textWriter;
+
+        internal CsvOutputWriter(TextWriter writer)
+        {
+            _textWriter = writer;
+        }
+
+        public void Write(IEnumerable<BandLikelihood> summary)
+        {
+            var outputBuilder = new StringBuilder();
+            outputBuilder.AppendLine("Forecast Throughput,Likelihood");
+            foreach(var band in summary)
+            {
+                outputBuilder.AppendLine(string.Join(",", $"At Least {band.Band} WUs", band.Likelihood.ToString("P3")));
+            }
+            _textWriter.Write(outputBuilder.ToString());
+        }
+    }
+}
