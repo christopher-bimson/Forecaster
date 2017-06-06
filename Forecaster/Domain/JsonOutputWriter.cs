4b949db0ba879a3e496d2360d311324fea18f743
Christopher Bimson <christopher.bimson>
2017-06-06 11:51:51 +0100

    Change the Json output to use the JavaScript camel case property convention.

--- Forecaster/Domain/JsonOutputWriter.cs
+++ Forecaster/Domain/JsonOutputWriter.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using System.Collections.Generic;
 using System.IO;
 
@@ -7,6 +8,12 @@ namespace Forecaster.Domain
     internal class JsonOutputWriter : IOutputWriter
     {
         private readonly TextWriter _writer;
+        private readonly JsonSerializerSettings _settings =
+            new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Formatting = Formatting.Indented
+            };
 
         internal JsonOutputWriter(TextWriter textWriter)
         {
@@ -15,7 +22,7 @@ namespace Forecaster.Domain
 
         public void Write(IEnumerable<BandLikelihood> summary)
         {
-            _writer.Write(JsonConvert.SerializeObject(summary, Formatting.Indented));
+            _writer.Write(JsonConvert.SerializeObject(summary, _settings));
         }
     }
 }
