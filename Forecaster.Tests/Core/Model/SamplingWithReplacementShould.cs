6627a686ec6644f47e66c38fc294b749c0ca0573
christopher-bimson <christopher-bimson>
2018-06-19 14:01:36 +0100

    Passing test for sampling without replacement extension method.

---
+++ Forecaster.Tests/Core/Model/SamplingWithReplacementShould.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Forecaster.Tests.Core.Model
+{
+    public class SamplingWithReplacementShould
+    {
+        [Fact]
+        public void Sample_All_Values_When_Using_A_Repeating_Rng()
+        {
+            var rng = new RepeatingRng(new[] { 0, 1, 2, 3, 4 });
+            
+            var samples = new[] { 1.0, 2.0, 3.0, 4.0, 5.0 };
+            var picked = samples.SampleWithReplacement(5, rng);
+
+            picked.Should().BeEquivalentTo(picked);
+        }
+    }
+}
---
+++ Forecaster/Core/Model/DoubleExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Forecaster.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System
+{
+    public static class DoubleExtensions
+    {
+        public static double[] SampleWithReplacement(this double[] samples, int count, 
+            IRng rng)
+        {
+            var result = new double[count];
+            for(int i = 0; i < count; i++)
+            {
+                result[i] = samples[rng.Next(samples.Length)];
+            }
+            return result;
+        }
+    }
+}
