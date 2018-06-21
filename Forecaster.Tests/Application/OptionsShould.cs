e1abaf3c4fa00c91afcb9728ba4afa368d4042dd
christopher-bimson <christopher-bimson>
2018-06-21 10:36:18 +0100

    Passing tests for valid arguments.

---
+++ Forecaster.Tests/Application/OptionsShould.cs
@@ -0,0 +1,33 @@
+﻿using CommandLine;
+using FluentAssertions;
+using Forecaster.Application;
+using Xunit;
+
+namespace Forecaster.Tests.Application
+{
+    public class OptionsShould
+    {
+        [Theory]
+        [InlineData("--samples 1 2 3 --forecast 5 --trials 1000", 
+            new[] { 1.0, 2.0, 3.0}, 5, 1000)]
+        [InlineData("-s 1 2 3 -f 5 -t 1000",
+            new[] { 1.0, 2.0, 3.0 }, 5, 1000)]
+        [InlineData("--samples 1 2 3 -f 5",
+            new[] { 1.0, 2.0, 3.0 }, 5, 10000)]
+        public void Parse_If_Syntactically_Valid(string args, double[] expectedSamples, 
+            int expectedForecast, int expectedTrials)
+        {
+            Parser.Default.ParseArguments<Options>(args.Split(' '))
+                .WithParsed(options => 
+                {
+                    options.Samples.Should().BeEquivalentTo(expectedSamples);
+                    options.Forecast.Should().Be(expectedForecast);
+                    options.Trials.Should().Be(expectedTrials);
+                })
+                .WithNotParsed(errors =>
+                {
+                    Assert.Empty(errors);
+                });
+        }
+    }
+}
---
+++ Forecaster/Application/Options.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Forecaster.Application
+{
+    public class Options
+    {
+        [Option('s', "samples", Required = true, Min = 1)]
+        public IEnumerable<double> Samples { get; set; }
+
+        [Option('f', "forecast", Required = true)]
+        public int Forecast { get; set; }
+
+        [Option('t', "trials", Required = false, Default = 10000)]
+        public int Trials { get; set; }
+    }
+}
