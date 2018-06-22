1da37ddf161e801a933009406f253bba31782d1f
christopher-bimson <christopher-bimson>
2018-06-22 12:36:58 +0100

    Quick sweep through tidying up unused using statements. I miss R#.

--- Forecaster/Application/Output/MarkdownRenderer.cs
+++ Forecaster/Application/Output/MarkdownRenderer.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using BetterConsoleTables;
-using Forecaster.Core.Model.Summary;
 
 namespace Forecaster.Application.Output
 {
